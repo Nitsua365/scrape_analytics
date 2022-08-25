952b90bef7a0b7b87c002345d446c18b6e61e34a
Austin Blanchard <42254605+Nitsua365@users.noreply.github.com>
2022-08-24 19:01:48 -0500

    changing data value

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,7 +3,14 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 import io from 'socket.io-client';
-import { LineChart, Line, XAxis, YAxis, Label } from 'recharts'
+import { 
+  LineChart, 
+  Line, 
+  XAxis, 
+  YAxis, 
+  Label
+} from 'recharts'
+
 
 const socket = io(`${process.env.REACT_APP_CONNECT_STRING}`, {
   transports: ['websocket', 'polling']
@@ -26,30 +33,35 @@ function App() {
     };
   }, []);
 
-  return (
-    <LineChart
-      width={700} 
-      height={300} 
-      data={cpuUsage} 
-    >
-      {(isConnected) ? 
-      (
-        <>
-          <XAxis dataKey='time'>
-            <Label value="Time" position="bottom" offset={-10} />
-          </XAxis>
-          <YAxis dataKey='percent' label={{ value: 'CPU Percent %', angle: -90, position: 'left' }} />
-          <Line type="monotone" dataKey="percent" stroke="#006eff" />
-        </>
-      ) : (
-        <>
-          <div>
-            Loading...
-          </div>
-        </>
-      )}
+  return ( cpuUsage.length &&
+    <>
+      <div>
+        <LineChart
+          width={700} 
+          height={300} 
+          data={cpuUsage}
+        >
+          {(isConnected) ? 
+          (
+            <>
+              <XAxis dataKey='time'>
+                <Label value="Time" position="bottom" offset={-10} />
+              </XAxis>
+              <YAxis dataKey='cpu.percent' label={{ value: 'CPU Percent %', angle: -90, position: 'centerLeft' }} />
+              <Line type="monotone" dataKey="cpu.percent" stroke="#006eff" animationDuration={400} />
+            </>
+          ) : (
+            <>
+              <div>
+                Loading...
+              </div>
+            </>
+          )}
 
-    </ LineChart>
+        </ LineChart>
+      </div>
+      {(cpuUsage.length) && <div>System Uptime: {cpuUsage[cpuUsage.length - 1].uptime}</div>}
+    </>
   );
 }
 
