c7b053d804f60ed939801fca725b27a590e50ee9
Nitsua365 <austinblanchard@hotmail.com>
2022-08-24 19:02:26 -0500

    added UPS battery level

--- back-end/index.js
+++ back-end/index.js
@@ -1,6 +1,9 @@
-const httpServer = require("http").createServer();
+const httpServer = require("http").createServer()
 const { Server } = require("socket.io");
 const cpu = require('cpu-percent')
+const os = require('os')
+const { exec } = require('node:child_process');
+var sudo = require('sudo-js');
 
 require('dotenv').config();
 
@@ -8,11 +11,26 @@ const io = new Server(httpServer, {
     transports: ['websocket', 'polling']
 });
 
+sudo.setPassword(process.env.PASSWORD);
+
 io.on('connection', (socket) => {
     cpu((error, percent) => {
         // console.log('per', percent)
-        if (error) console.error('error', error)
-        else socket.emit('CPUPercent', { percent, time: new Date(Date.now()).toLocaleTimeString('en-US', { hour12: false }) })
+        if (error) console.error('cpu error', error)
+        else {
+            // get Battery life of UPS
+            sudo.exec(['sudo', 'pwrstat', '-status'], (err, stdout, stderr) => {
+                const batteryRegx = / [0-9]+ %/g
+                // console.log(stdout)
+                let data = { 
+                    cpu : { percent },
+                    uptime: os.uptime(),
+                    time: new Date(Date.now()).toLocaleTimeString('en-US', { hour12: false }),
+                    batteryLvl: stderr.match(batteryRegx)[0].split(' ')[1]
+                };
+                socket.emit('CPUPercent', data)
+            })
+        }
     }, 3000)
 });
 
