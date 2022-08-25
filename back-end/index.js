92736683056d4d860533f11dcc7da20710e2dfa9
Nitsua365 <austinblanchard@hotmail.com>
2022-08-24 23:26:00 -0500

    refactor polling and added emit on initial connect

--- back-end/index.js
+++ back-end/index.js
@@ -11,27 +11,48 @@ const io = new Server(httpServer, {
     transports: ['websocket', 'polling']
 });
 
+// set the password for sudo commands
 sudo.setPassword(process.env.PASSWORD);
 
+const BATTERY_REGEX = /[0-9]+ %/g
+
 io.on('connection', (socket) => {
-    cpu((error, percent) => {
+
+    const UPS_Analytics = () => {
+        sudo.exec(['sudo', 'pwrstat', '-status'], (err, pid, output) => {
+            if (err) console.error('UPS pwrstat error:', err, 'pid:', pid)
+            socket.emit('UPS', {
+                batteryLvl: output.match(BATTERY_REGEX)[0].split(' ')[0],
+                load: {
+                    percent: output.match(BATTERY_REGEX)[1].split(' ')[0],
+                    wattage: output.match(/Load\.* [0-9]*/g)[0].split(' ')[1]
+                },
+                state: output.match(/State\.* [a-zA-Z0-9]*/g)[0].split(' ')[1]
+            })
+        })
+    }
+    
+    const CPU_Analytics = (error, percent) => {
         // console.log('per', percent)
         if (error) console.error('cpu error', error)
         else {
-            // get Battery life of UPS
-            sudo.exec(['sudo', 'pwrstat', '-status'], (err, stdout, stderr) => {
-                const batteryRegx = / [0-9]+ %/g
-                // console.log(stdout)
-                let data = { 
-                    cpu : { percent },
-                    uptime: os.uptime(),
-                    time: new Date(Date.now()).toLocaleTimeString('en-US', { hour12: false }),
-                    batteryLvl: stderr.match(batteryRegx)[0].split(' ')[1]
-                };
-                socket.emit('CPUPercent', data)
+            socket.emit('CPU', { 
+                cpu : { percent },
+                uptime: os.uptime(),
+                time: new Date(Date.now()).toLocaleTimeString('en-US', { hour12: false }),
             })
         }
-    }, 3000)
+    }
+
+    // send the initial polling data on connection
+    UPS_Analytics();
+    cpu(CPU_Analytics)();
+
+    // CPU analytics
+    cpu(CPU_Analytics, 3000)
+
+    // UPS analytics
+    setInterval(UPS_Analytics, 30000)
 });
 
 httpServer.listen(process.env.PORT, () => console.log(`listening on ${process.env.PORT}`))
